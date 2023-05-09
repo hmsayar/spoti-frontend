4faff5d6b58522861e92944e0a33979a3a8a3838
mert sayar <merttsayar@gmail.com>
2023-05-09 16:32:31 +0300

    readme

--- README.md
+++ README.md
@@ -1,3 +1,50 @@
-# spoti-frontend
+# Spotify Frontend Clone
 
-https://spoti-frontend.onrender.com/
+A React-based frontend clone of Spotify, replicating most of its features and utilizing the Spotify API and Web Playback SDK.
+
+## Motivation
+
+The primary motivation behind this project was to learn React.js by implementing a complex, real-world application. Additionally, it provided experience in using related libraries like react-router-dom and axios.
+
+## Live Demo
+
+You can try the live demo here: https://spoti-frontend.onrender.com/
+For access to authenticated features, please send me an email so I can add your user information to the project.
+
+## Upcoming Version
+
+Currently, I am working on an updated version of the project, utilizing Next.js 13 with app router.
+
+
+## Installation and Setup
+
+1. Set up the [Spotify Backend](https://github.com/hmsayar/spoti-backend.git) following the instructions in its README.
+
+2. Clone the repository: https://github.com/hmsayar/spoti-frontend.git
+
+3. Change to the project directory: cd spoti-frontend
+
+4. Create a `.env.development` file in the root directory with the following variables:
+
+    ```plaintext
+    VITE_APP_BACK_URI = "http://localhost:4000"
+
+5. Install dependencies: npm install
+
+6. Start the development server: npm start
+
+7. Open your browser and navigate to `http://localhost:5173`.
+
+8. Ensure the backend server is running on `http://localhost:4000`
+
+## Technologies Used
+
+- React.js
+- react-router-dom
+- Vite
+- Axios
+
+
+## Note
+
+While developing this project, my primary focus was on learning and exploring React.js, which led to implementing a wide range of features. As a result, some parts of the codebase may not be optimally implemented. I see this project as a representation of my learning journey, and I'm continually working on refining the codebase and improving my skills.
--- src/components/maincomponents/Home.jsx
+++ src/components/maincomponents/Home.jsx
@@ -5,98 +5,96 @@ import getLocale from "../utils/locale"
 import UserTopItems from "./UserTopItems"
 import { LoginContext } from "../../context/loginContext"
 import useResize from "../../hooks/useResize"
+import Loading from "./Loading"
 
 
 
 export default function Home() {
 
-    const [allData, setAllData] = useState([])
-    const { login } = useContext(LoginContext)
-    const [isLoading, setIsLoading] = useState(true)
-    const [columnCount, setColumnCount] = useState(getColumnCount(window.innerWidth))
-    const [screenWidth] = useResize()
+  const [allData, setAllData] = useState([])
+  const { login } = useContext(LoginContext)
+  const [isLoading, setIsLoading] = useState(true)
+  const [columnCount, setColumnCount] = useState(getColumnCount(window.innerWidth))
+  const [screenWidth] = useResize()
 
-    useEffect(() => {
-        let source = axios.CancelToken.source()
-        const [language, locale] = getLocale()
-        let endpoint = `https://api.spotify.com/v1/browse/categories?limit=6&country=${locale}&locale=${language}_${locale}`
-        const makeRequest = async () => {
-            const cancelToken = source.token
-            const config = {
-                method: 'POST',
-                url: `${import.meta.env.VITE_APP_BACK_URI}`,
-                data: { endpoint },
-                withCredentials: true,
-                cancelToken
-            }
-            try {
-                var result = await axios(config)
-                setAllData(result.data.categories.items)
-                 setIsLoading(false)
-            } catch (error) {
-                if (axios.isCancel(error)) return
-                return error
-            }
+  useEffect(() => {
+    let source = axios.CancelToken.source()
+    const [language, locale] = getLocale()
+    let endpoint = `https://api.spotify.com/v1/browse/categories?limit=6&country=${locale}&locale=${language}_${locale}`
+    const makeRequest = async () => {
+      const cancelToken = source.token
+      const config = {
+        method: 'POST',
+        url: `${import.meta.env.VITE_APP_BACK_URI}`,
+        data: { endpoint },
+        withCredentials: true,
+        cancelToken
+      }
+      try {
+        var result = await axios(config)
+        setAllData(result.data.categories.items)
+        setIsLoading(false)
+      } catch (error) {
+        if (axios.isCancel(error)) return
+        return error
+      }
 
-        }
-        makeRequest()
+    }
+    makeRequest()
 
-    }, [])
+  }, [])
 
-    useEffect(() => {
-        setColumnCount(getColumnCount(screenWidth));
-      }, [screenWidth]);
-    
-
-    const rowElements = allData.map(item => {
-        return (
-
-            <div key={item.id}>
-                <h1 className="row-title">{item.name}</h1>
-                <RowContent data={item} columnCount={columnCount} />
-            </div>
-        )
-    })
+  useEffect(() => {
+    setColumnCount(getColumnCount(screenWidth));
+  }, [screenWidth]);
 
 
+  const rowElements = allData.map(item => {
     return (
-        <div className="main-content">
-            
 
-                    <div className="home-main-content" >
-                        {/* <LoadingImg style={{ height: "180px", width: "180px", backgroundColor: "rgba(94,94,94,1)", borderRadius: "10px" }}  /> */}
-                        <>
-                            {login ?
-                                <UserTopItems /> :
-                                null
-                            }
-
-
-                            {rowElements}
-
-                        </>
-                    </div>
-            
-
-        </div>
+      <div key={item.id}>
+        <h1 className="row-title">{item.name}</h1>
+        <RowContent data={item} columnCount={columnCount} />
+      </div>
     )
+  })
+
+  if (isLoading) {
+    return (<Loading />)
+  } else {
+    return (
+      <div className="main-content">
+        <div className="home-main-content" >
+          {/* <LoadingImg style={{ height: "180px", width: "180px", backgroundColor: "rgba(94,94,94,1)", borderRadius: "10px" }}  /> */}
+          <>
+            {login ?
+              <UserTopItems /> :
+              null
+            }
+            {rowElements}
+          </>
+        </div>
+      </div>
+    )
+  }
 }
 
 
-function getColumnCount(width){
-    if (width <= 950) {
-        return 2;
-      } else if (width <= 1120) {
-        return 3;
-      } else if (width <=1300) {
-        return 4;
-      } else if (width <=1540) {
-        return 5;
-      } else if(width <= 1750) {
-        return 6;
-      }else if(width <= 1950) {
-        return 7;
-      }else{
-        return 8;
-      }
+
+function getColumnCount(width) {
+  if (width <= 950) {
+    return 2;
+  } else if (width <= 1120) {
+    return 3;
+  } else if (width <= 1300) {
+    return 4;
+  } else if (width <= 1540) {
+    return 5;
+  } else if (width <= 1750) {
+    return 6;
+  } else if (width <= 1950) {
+    return 7;
+  } else {
+    return 8;
+  }
 }
